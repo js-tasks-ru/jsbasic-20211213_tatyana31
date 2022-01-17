5e398ac1f6cf2f0ea8dd1e8e27950f52c13e347d
Tianamellon <tianamellon@gmail.com>
2022-01-17 23:39:57 +0300

    небольшой фикс

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,9 +2,9 @@ function sumSalary(salaries) {
   let systemFields = ["month", "currency", "isPayed"];
   let invalidValues = [NaN, Infinity, -Infinity];
   let result = 0;
-  
+
   for (let key in salaries) { 
-    if (!systemFields.includes(key) && typeof salaries[key] == "number" && !invalidValues.includes(salaries[key])) { 
+    if (!systemFields.includes(key) && typeof salaries[key] === "number" && !invalidValues.includes(salaries[key])) { 
       result += salaries[key];
     }
   }
