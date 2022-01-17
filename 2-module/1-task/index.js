889dbbab87f1003a1e41df1e5ca26fc887f6dc63
Tianamellon <tianamellon@gmail.com>
2022-01-17 20:39:27 +0300

    Функция sumSalary(salaries) - решение

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let systemFields = ["month", "currency", "isPayed"];
+  let invalidValues = [NaN, Infinity, -Infinity];
+  let result = 0;
+  
+  for (let key in salaries) { 
+    if (!systemFields.includes(key) && typeof salaries[key] == "number" && !invalidValues.includes(salaries[key])) { 
+      result += salaries[key];
+    }
+  }
+
+  return result;
 }
