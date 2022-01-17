7455c81e4874c5d2211945ffc4d15e6b08b62691
Tianamellon <tianamellon@gmail.com>
2022-01-17 20:48:05 +0300

    Создайте калькулятор - решение

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+  read: function (paramA, paramB) { 
+    this.a = paramA;
+    this.b = paramB;
+  },
+  sum: function () { 
+    return this.a + this.b;
+  },
+  mul: function () { 
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
