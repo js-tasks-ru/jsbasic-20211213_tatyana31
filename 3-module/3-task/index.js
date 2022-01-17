3cf4925c29e6060a5ce936dae3412cea2cf0c6a7
Tianamellon <tianamellon@gmail.com>
2022-01-17 23:40:29 +0300

    Переведите текст вида border-left-width в borderLeftWidth - решение

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let parts = str.split("-");
+  let result = parts[0];
+
+  for (let i = 1; i < parts.length; i++) { 
+    result += parts[i].substring(0, 1).toUpperCase() + parts[i].substring(1);
+  }
+
+  return result;
 }
