478afa2218702b93e90d4e044b60076589a54f33
Tianamellon <tianamellon@gmail.com>
2022-01-30 17:50:37 +0300

    Скрыть элемент по нажатию кнопки - решение

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+
+  btn.onclick = function () { 
+    var text = document.querySelector('#text');
+
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+    } else { 
+      text.setAttribute('hidden', true);
+    }
+  }
 }
